adada8193f3fa7111a82cbbd20410cffaecf2e3e
gianlucafalaschi <gian100gian@outlook.it>
2024-03-13 15:25:09 +0100

    feat: query selector

---
+++ for-cicle/js/script.js
@@ -0,0 +1,23 @@
+/* // Data una lista della spesa:
+
+Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+Consigli:
+- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while */
+
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+// selezionare elemento lista ul in html
+let ulList = document.querySelector('ul.list');
+console.log(ulList);
+// CON CICLO FOR
+// per ogni elemento dell'array
+    //stampare in html l'elemento
